33a5f8a702376699d05898360c10eaf025a6e857
AGLIATI FEDERICO <AGLIATI.21076>
2026-02-02 09:17:07 +0100

    blud

--- index.php
+++ index.php
@@ -6,9 +6,15 @@ require_once "config.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Valutazione rischi</title>
+    <title>Document</title>
 </head>
 <body>
-    
+    <div id="ins_tab">
+        <table>
+            <tr><th>PhonkAura</th><th>Dato</th></tr>
+            <tr><td>Nico Uovo</td><td>dato</td></tr>
+
+        </table>
+    </div>
 </body>
 </html>
