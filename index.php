bdb213ee633483379d9a951ebc96bdd77b35e4f4
Federico Agliati <145464011+Uccetta110@users.noreply.github.com>
2026-02-07 15:16:20 +0000

    starting index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-// login page, use config.php to access database
+// login page, use config.php to access database/*
+/*
 require_once 'config.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = trim($_POST['username'] ?? '');
@@ -21,3 +22,111 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 }
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="/action_page.php">
+        <table>
+            <tr>
+                <th>
+                    Dato
+                </th>
+                <th>
+                    Inserimento
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    Sesso:
+                </td>
+                <td>
+                    <input type="radio" name="sesso" value="M">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Età:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Altezza mani sollevamento:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Altezza mani verticale:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Altezza mani orizzontale:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Dislocazione angolare:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Giudizio presa carico:
+                </td>
+                <td>
+                    INT
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Frequenza gesti:
+                </td>
+                <td>
+                    Decimal
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Frequenza lavoro:
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Peso sollevato:
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
